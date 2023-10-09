65952d8c1ddf641875a63416dd96a587d46200cd
Wyatt Murray <wyattmurray1235@gmail.com>
2023-10-09 14:05:31 -0400

    boss health start

--- root/Team1Project2/Assets/1 - Scripts/Enemies/BossHealthManager.cs
+++ root/Team1Project2/Assets/1 - Scripts/Enemies/BossHealthManager.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
-public class BossHealthManager : MonoBehaviour
+public class BossHealthManager : EnemyHealthmanager
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public struct CrystalInfo
     {
-        
+        public GameObject[] crystal;
+        public Vector3[] crystalPositions;
+        public Transform crystalParent;
+        public GameObject cystalPrefab;
+        public int crystalsLeft;
+        public int maxCrystals;
+        public bool useDebugTools;
     }
 
-    // Update is called once per frame
-    void Update()
+    [Space(2)]
+    [Header("CrystalInfo")]
+    [Space(1)]
+
+    [SerializeField] private CrystalInfo crystalInfo;
+
+
+    protected override void Start()
     {
-        
+
+
+        SetCrystalPositions();
+        m_canTakeDamage = false;
     }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (crystalInfo.useDebugTools)
+        {
+            SetCrystalPositions();
+        }
+    }
+
+    private void SetCrystalPositions()
+    {
+        crystalInfo.crystalPositions = new Vector3[crystalInfo.crystal.Length];
+        int index = 0;
+        foreach (GameObject crystalObject in crystalInfo.crystal)
+        {
+            Vector3 pos = crystalObject.transform.position;
+            crystalInfo.crystalPositions[index] = pos;
+            index++;
+        }
+    }
+
+    private void PlaceCrystals()
+    {
+        crystalInfo.crystalsLeft = crystalInfo.maxCrystals;
+        foreach (Vector3 pos in crystalInfo.crystalPositions)
+        {
+            Instantiate(crystalInfo.cystalPrefab, crystalInfo.crystalParent.position, Quaternion.identity);
+        }
+    }
+
+    public void BreakOneCrystal()
+    {
+        crystalInfo.crystalsLeft--;
+        //start a coroutine to show the boss took some hits
+        //if the boss has crystyals
+    }
+
+
 }
